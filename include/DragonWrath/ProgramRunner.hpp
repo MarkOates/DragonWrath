c5bc883a9d559b816dc7be50579ebd134872ebd4
Mark Oates <markoates0@gmail.com>
2020-12-13 23:27:54 -0500

    Move all required objects into method call

--- include/DragonWrath/ProgramRunner.hpp
+++ include/DragonWrath/ProgramRunner.hpp
@@ -1,19 +1,12 @@
 #pragma once
 
 #include <allegro5/allegro.h>
-#include <AllegroFlare/Framework.hpp>
-#include <AllegroFlare/Display.hpp>
-#include <DragonWrath/GameplayScreen.hpp>
 
 namespace DragonWrath
 {
    class ProgramRunner
    {
    private: 
-      AllegroFlare::Display *display;
-      AllegroFlare::Screens screens;
-      AllegroFlare::Framework framework;
-      DragonWrath::GameplayScreen gameplay_screen;
 
    public:
       ProgramRunner();
--- src/DragonWrath/ProgramRunner.cpp
+++ src/DragonWrath/ProgramRunner.cpp
@@ -1,14 +1,12 @@
 #include <DragonWrath/ProgramRunner.hpp>
 
+#include <AllegroFlare/Framework.hpp>
+#include <AllegroFlare/Display.hpp>
+#include <DragonWrath/GameplayScreen.hpp>
 #include <iostream>
 
 DragonWrath::ProgramRunner::ProgramRunner()
-   : display(nullptr)
-   , screens()
-   , framework(screens)
-   , gameplay_screen(framework)
 {
-   std::cout << "DragonWrath initialized successfully." << std::endl;
 }
 
 DragonWrath::ProgramRunner::~ProgramRunner()
@@ -17,6 +15,14 @@ DragonWrath::ProgramRunner::~ProgramRunner()
 
 void DragonWrath::ProgramRunner::run()
 {
+   std::cout << "DragonWrath run started successfully." << std::endl;
+
+   AllegroFlare::Display *display;
+   AllegroFlare::Screens screens;
+   AllegroFlare::Framework framework(screens);
+
+   DragonWrath::GameplayScreen gameplay_screen(framework);
+
    framework.initialize();
    display = framework.create_display(AllegroFlare::Display::RESOLUTION_HD_1080);
 
