d818d85e889e988f5b38b9de9cf0ac06ac239c4c
Mark Oates <markoates0@gmail.com>
2020-12-16 17:51:03 -0500

    Add Levels::TimedScroll scaffold

---
+++ include/DragonWrath/Levels/TimedScroll.hpp
@@ -0,0 +1,17 @@
+#pragma once
+
+#include <DragonWrath/Levels/Base.hpp>
+
+namespace DragonWrath
+{
+   namespace Levels
+   {
+      class TimedScroll : public DragonWrath::Levels::Base
+      {
+      private:
+      public:
+         TimedScroll();
+         ~TimedScroll();
+      };
+   }
+}
---
+++ src/DragonWrath/Levels/TimedScroll.cpp
@@ -0,0 +1,23 @@
+
+#include <DragonWrath/Levels/TimedScroll.hpp>
+
+
+namespace DragonWrath
+{
+namespace Levels
+{
+
+
+TimedScroll::TimedScroll()
+   : DragonWrath::Levels::Base(100)
+{}
+
+
+TimedScroll::~TimedScroll()
+{}
+
+
+} // namespace Levels
+} // namespace DragonWrath
+
+
