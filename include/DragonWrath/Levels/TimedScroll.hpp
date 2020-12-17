726de85ea4028ba0080bb7ed4e7971c5c4b121d4
Mark Oates <markoates0@gmail.com>
2020-12-16 19:56:54 -0500

    Add EnemyToSpawn class and "enemies_to_spawn" property on TimedScroll

--- include/DragonWrath/Levels/TimedScroll.hpp
+++ include/DragonWrath/Levels/TimedScroll.hpp
@@ -2,6 +2,22 @@
 
 #include <DragonWrath/Levels/Base.hpp>
 
+
+class EnemyToSpawn
+{
+public:
+  bool spawned;
+  float spawn_time;
+  std::string enemy_type;
+  float spawn_x;
+  float spawn_y;
+  std::string movement_strategy;
+
+  EnemyToSpawn(float spawn_time, std::string enemy_type, float spawn_x, float spawn_y, std::string movement_strategy);
+  ~EnemyToSpawn();
+};
+
+
 namespace DragonWrath
 {
    namespace Levels
@@ -11,9 +27,10 @@ namespace DragonWrath
       private:
          float timer;
          const float timer_step;
+         std::vector<EnemyToSpawn> enemies_to_spawn;
 
       public:
-         TimedScroll();
+         TimedScroll(std::vector<EnemyToSpawn> enemies_to_spawn={});
          ~TimedScroll();
 
          void update() override;
--- src/DragonWrath/Levels/TimedScroll.cpp
+++ src/DragonWrath/Levels/TimedScroll.cpp
@@ -4,16 +4,30 @@
 #include <allegro5/allegro.h>
 
 
+EnemyToSpawn::EnemyToSpawn(float spawn_time, std::string enemy_type, float spawn_x, float spawn_y, std::string movement_strategy)
+   : spawn_time(spawn_time)
+   , enemy_type(enemy_type)
+   , spawn_x(spawn_x)
+   , spawn_y(spawn_y)
+   , movement_strategy(movement_strategy)
+{}
+
+
+EnemyToSpawn::~EnemyToSpawn()
+{}
+
+
 namespace DragonWrath
 {
 namespace Levels
 {
 
 
-TimedScroll::TimedScroll()
+TimedScroll::TimedScroll(std::vector<EnemyToSpawn> enemies_to_spawn)
    : DragonWrath::Levels::Base(TIMED_SCROLL)
    , timer(0)
    , timer_step(ALLEGRO_BPS_TO_SECS(60))
+   , enemies_to_spawn(enemies_to_spawn)
 {}
 
 
