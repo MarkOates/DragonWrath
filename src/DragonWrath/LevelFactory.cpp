e53a77264d78b7d772b201a7119383a5188ce160
Mark Oates <markoates0@gmail.com>
2020-12-17 19:07:12 -0500

    Create fuller list of enemies that spawn in test level

--- src/DragonWrath/LevelFactory.cpp
+++ src/DragonWrath/LevelFactory.cpp
@@ -38,11 +38,16 @@ DragonWrath::Levels::TimedScroll *LevelFactory::create_timed_scroll_level_with_t
    // enemies to create
   // EnemyToSpawn(float spawn_time, std::string enemy_type, float spawn_x, float spawn_y, std::string movement_strategy);
    std::vector<EnemyToSpawn> enemies_to_spawn = {
-      { 1, "doesnt-matter-right-now", 1920*0.75 - 300, 1080/2 - 100, MOVE_LEFT},
-      { 2, "doesnt-matter-right-now", 1920*0.75 - 150, 1080/2 - 50,  MOVE_RIGHT},
-      { 3, "doesnt-matter-right-now", 1920*0.75 + 0,   1080/2 + 0,   SIN_WAVE_MOVE_LEFT},
-      { 4, "doesnt-matter-right-now", 1920*0.75 + 150, 1080/2 + 50,  MOVE_RIGHT},
-      { 5, "doesnt-matter-right-now", 1920*0.75 + 300, 1080/2 + 100, MOVE_LEFT},
+      { 0, "doesnt-matter-right-now", 1920*0.75 - 300, 1080/2 - 100, MOVE_LEFT},
+      { 0.5, "doesnt-matter-right-now", 1920*0.75 - 150, 1080/2 - 50,  MOVE_LEFT},
+      { 1, "doesnt-matter-right-now", 1920*0.75 + 0,   1080/2 + 0,   MOVE_LEFT},
+      { 1.5, "doesnt-matter-right-now", 1920*0.75 + 150, 1080/2 + 50,  MOVE_LEFT},
+      { 2, "doesnt-matter-right-now", 1920*0.75 + 300, 1080/2 + 100, MOVE_LEFT},
+      { 2.5, "doesnt-matter-right-now", 1920*0.75 - 300, 1080/2 - 100, MOVE_LEFT},
+      { 3, "doesnt-matter-right-now", 1920*0.75 - 150, 1080/2 - 50,  MOVE_LEFT},
+      { 3.5, "doesnt-matter-right-now", 1920*0.75 + 0,   1080/2 + 0,   MOVE_LEFT},
+      { 4, "doesnt-matter-right-now", 1920*0.75 + 150, 1080/2 + 50,  MOVE_LEFT},
+      { 4.5, "doesnt-matter-right-now", 1920*0.75 + 300, 1080/2 + 100, MOVE_LEFT},
    };
 
    DragonWrath::Levels::TimedScroll *timed_scroll_level =
