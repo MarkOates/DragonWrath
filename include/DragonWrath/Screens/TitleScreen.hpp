5371a4f488f354b462759eb32d4b03800a7a3bee
Mark Oates <markoates0@gmail.com>
2020-12-18 20:30:21 -0500

    Add TitleScreen scaffold

---
+++ include/DragonWrath/Screens/TitleScreen.hpp
@@ -0,0 +1,18 @@
+#pragma once
+
+#include <DragonWrath/Screens/Base.hpp>
+
+namespace DragonWrath
+{
+   namespace Screens
+   {
+      class TitleScreen : public DragonWrath::Screens::Base
+      {
+      public:
+         TitleScreen();
+         ~TitleScreen();
+      };
+   }
+}
+
+
---
+++ src/DragonWrath/Screens/TitleScreen.cpp
@@ -0,0 +1,25 @@
+
+
+#include <DragonWrath/Screens/TitleScreen.hpp>
+
+
+namespace DragonWrath
+{
+namespace Screens
+{
+
+
+TitleScreen::TitleScreen()
+{
+}
+
+
+TitleScreen::~TitleScreen()
+{
+}
+
+
+} // Screens
+} // DragonWrath
+
+
