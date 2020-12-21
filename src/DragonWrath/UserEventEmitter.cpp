3b946b7c4910fc4ad48cd27c641a810aa6ec043f
Mark Oates <markoates0@gmail.com>
2020-12-20 22:41:11 -0500

    Extract "validate_screen_switcher_event_source_is_present" for error messages

--- src/DragonWrath/UserEventEmitter.cpp
+++ src/DragonWrath/UserEventEmitter.cpp
@@ -17,19 +17,14 @@ UserEventEmitter::~UserEventEmitter()
 {}
 
 
-void UserEventEmitter::emit_start_title_screen_event()
+void UserEventEmitter::validate_screen_switcher_event_souce_is_present(std::string method_name)
 {
-   if (screen_switcher_event_souce)
-   {
-      ALLEGRO_EVENT event;
-      event.user.type = SCREEN_MANAGER_SWITCH_SCREEN_EVENT;
-      event.user.data1 = 1;
-      al_emit_user_event(screen_switcher_event_souce, &event, NULL);
-   }
-   else
+   if (!screen_switcher_event_souce)
    {
       std::stringstream error_message;
-      error_message << "UserEventEmitter::emit_start_title_screen_event(): "
+      error_message
+         << "UserEventEmitter::"
+         << method_name
          << "error: screen_switcher_event_souce cannot be nullptr.\""
          << std::endl;
       throw std::runtime_error(error_message.str());
@@ -37,23 +32,25 @@ void UserEventEmitter::emit_start_title_screen_event()
 }
 
 
+void UserEventEmitter::emit_start_title_screen_event()
+{
+   validate_screen_switcher_event_souce_is_present("emit_start_title_screen_event");
+
+   ALLEGRO_EVENT event;
+   event.user.type = SCREEN_MANAGER_SWITCH_SCREEN_EVENT;
+   event.user.data1 = 1;
+   al_emit_user_event(screen_switcher_event_souce, &event, NULL);
+}
+
+
 void UserEventEmitter::emit_start_gameplay_screen_event()
 {
-   if (screen_switcher_event_souce)
-   {
-      ALLEGRO_EVENT event;
-      event.user.type = SCREEN_MANAGER_SWITCH_SCREEN_EVENT;
-      event.user.data1 = 2;
-      al_emit_user_event(screen_switcher_event_souce, &event, NULL);
-   }
-   else
-   {
-      std::stringstream error_message;
-      error_message << "UserEventEmitter::emit_start_gameplay_screen_event(): "
-         << "error: screen_switcher_event_souce cannot be nullptr.\""
-         << std::endl;
-      throw std::runtime_error(error_message.str());
-   }
+   validate_screen_switcher_event_souce_is_present("emit_start_gameplay_screen_event");
+
+   ALLEGRO_EVENT event;
+   event.user.type = SCREEN_MANAGER_SWITCH_SCREEN_EVENT;
+   event.user.data1 = 2;
+   al_emit_user_event(screen_switcher_event_souce, &event, NULL);
 }
 
 
