4400a800d86b5958341ceb1e1d2b984284732057
Mark Oates <markoates0@gmail.com>
2020-12-23 22:51:21 -0500

    Introduce TrackPlayerDragon movement strategy; Add to PurpleDragon

--- src/DragonWrath/Entities/Enemies/PurpleDragon.cpp
+++ src/DragonWrath/Entities/Enemies/PurpleDragon.cpp
@@ -3,7 +3,7 @@
 #include <DragonWrath/Entities/Enemies/PurpleDragon.hpp>
 
 #include <DragonWrath/MovementStrategyNames.hpp>
-#include <DragonWrath/Entities/MovementStrategies/MoveAlongPath.hpp>
+#include <DragonWrath/Entities/MovementStrategies/TrackPlayerDragon.hpp>
 #include <sstream>
 #include <math.h>
 
@@ -18,7 +18,7 @@ namespace Enemies
 
 PurpleDragon::PurpleDragon(ElementID *parent, float x, float y)
    : DragonWrath::Entities::Enemies::Base(parent, "basic_enemy", x, y, 3)
-   , movement_strategy(new DragonWrath::Entities::MovementStrategies::MoveAlongPath(this, 10))
+   , movement_strategy(new DragonWrath::Entities::MovementStrategies::TrackPlayerDragon(this, 2))
 {
 }
 
--- src/DragonWrath/Entities/MovementStrategies/TrackPlayerDragon.cpp
+++ src/DragonWrath/Entities/MovementStrategies/TrackPlayerDragon.cpp
@@ -2,7 +2,10 @@
 
 #include <DragonWrath/Entities/MovementStrategies/TrackPlayerDragon.hpp>
 
+#include <DragonWrath/Levels/Base.hpp>
+#include <DragonWrath/SceneCollectionHelper.hpp>
 #include <allegro_flare/path2d.h>
+#include <sstream>
 
 
 namespace DragonWrath
@@ -30,7 +33,33 @@ void TrackPlayerDragon::update()
 {
    if (entity)
    {
-      // logic TODO here
+      AllegroFlare::ElementID *root = entity->get_root();
+      if (root)
+      {
+         // this is making the assumption that the root will always be the DragonWrath::Levels::Base*
+         DragonWrath::Levels::Base *level = static_cast<DragonWrath::Levels::Base *>(root);
+         DragonWrath::SceneCollectionHelper scene_collection_helper(level);
+         DragonWrath::Entities::PlayerDragon *player_dragon = scene_collection_helper.get_player_dragon();
+
+         if (player_dragon)
+         {
+            AllegroFlare::vec2d direction_vector =
+               (player_dragon->place.position - entity->place.position).normalized();
+            entity->velocity.position = direction_vector * speed;
+         }
+         else
+         {
+            std::stringstream error_message;
+            error_message << "TrackPlayerDragon::update(): error: player_dragon is unexpectedly nullptr" << std::endl;
+            throw std::runtime_error(error_message.str());
+         }
+      }
+      else
+      {
+         std::stringstream error_message;
+         error_message << "TrackPlayerDragon::update(): error: root is unexpectedly nullptr" << std::endl;
+         throw std::runtime_error(error_message.str());
+      }
    }
 }
 
