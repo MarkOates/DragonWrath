d690092d40b2c7fb254ac840020d1232a69a09d3
Mark Oates <markoates0@gmail.com>
2020-12-30 20:12:39 -0500

    Add MotionFX::Base

---
+++ include/DragonWrath/Entities/MotionFX/Base.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+
+#include <DragonWrath/Entities/Base.hpp>
+
+
+namespace DragonWrath
+{
+   namespace MotionFX
+   {
+      class Base : public DragonWrath::Entities::Base
+      {
+      protected:
+         float duration;
+         float counter;
+
+      public:
+         Base(ElementID *parent, std::string motion_fx_type, float x, float y, float duration);
+         virtual ~Base();
+
+         virtual void update();
+      };
+   }
+}
+
+
---
+++ src/DragonWrath/Entities/MotionFX/Base.cpp
@@ -0,0 +1,35 @@
+
+
+#include <DragonWrath/Entities/MotionFX/Base.hpp>
+
+
+namespace DragonWrath
+{
+namespace MotionFX
+{
+
+
+Base::Base(ElementID *parent, std::string motion_fx_type, float x, float y, float duration)
+   : Entities::Base(parent, "motion_fx", x, y)
+   , duration(duration)
+   , counter(duration)
+{
+   set("motion_fx_type", motion_fx_type);
+}
+
+
+Base::~Base() {}
+
+
+void Base::update()
+{
+   counter -= 1.0 / 60.0;
+   if (counter <= 0) flag_for_deletion();
+}
+
+
+} // namespace MotionFX
+} // namespace DragonWrath
+
+
+
